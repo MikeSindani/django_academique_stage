f9c5f67ec54e5311481831d9206786c2d1189c43
Mike Sindani <mikems@live.fr>
2024-07-03 12:32:35 +0200

    add dashbord list universite

--- dashbord/views.py
+++ dashbord/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from .models import Etudiant, Universite, Entreprise
 
+
 # Create your views here.
-def dashboard (request):
+def dashboard(request):
     totals__entreprise = Entreprise.objects.all().count()
     totals__universite = Universite.objects.all().count()
-    return render(request, 'dashboard/dashboard.html',{'totals__universite':totals__universite,'totals__entreprise':totals__entreprise})
+    entreprises = Entreprise.objects.all()
+    return render(
+        request,
+        "dashboard/dashboard.html",
+        {
+            "totals__universite": totals__universite,
+            "totals__entreprise": totals__entreprise,
+            "entreprises": entreprises,
+        },
+    )
