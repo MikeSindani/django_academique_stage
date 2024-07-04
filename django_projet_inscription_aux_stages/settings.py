a03e4934d47bf836d168457993a2523a95e5b1d1
Mike Sindani <mikems@live.fr>
2024-07-04 12:19:24 +0200

    mis en place de wjhitenoise

--- django_projet_inscription_aux_stages/settings.py
+++ django_projet_inscription_aux_stages/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'django_projet_inscription_aux_stages.urls'
